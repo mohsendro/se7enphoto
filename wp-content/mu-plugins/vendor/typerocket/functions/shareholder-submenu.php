e9cd60c111d259adb6ad93b4d0b127bb589e534d
mohsendro <mohsendroo@yahoo.com>
2023-02-04 23:36:32 +0330

    finished wc-shareholder table v1.0.0

--- wp-content/mu-plugins/vendor/typerocket/functions/shareholder-submenu.php
+++ wp-content/mu-plugins/vendor/typerocket/functions/shareholder-submenu.php
@@ -15,100 +15,33 @@ function shareholder_wc_register_shareholder_submenu_page() {
       );
 
       // screen option
-      add_action("load-$hook", 'shareholder_table_add_options');
-
       function shareholder_table_add_options() {
 
             $args_page = array(
                   'label' => 'تعداد موردها در هر برگه:',
                   'default' => 20,
-                  'option' => 'employees_per_page'
+                  'option' => 'shareholder_per_page'
             );
 
-            // $args_columns = array(
-            //       'label' => 'ستون‌ها',
-            //       'max' => 10,
-            //       'default' => 2,
-            //       // 'option' => 'employees_per_page'
-            // );
             add_screen_option('per_page', $args_page);
-            // add_screen_option('layout_columns', $args_columns);
-
-            // $empTable = new Employees_List_Table();
 
       }
+      add_action("load-$hook", 'shareholder_table_add_options');
       
 }
 add_action('admin_menu', 'shareholder_wc_register_shareholder_submenu_page');
+
+// get saved screen meta value
+function shareholder_table_set_option($status, $option, $value) {
+
+      return $value;
+
+}
+add_filter('set-screen-option', 'shareholder_table_set_option', 10, 3);
 
 function shareholder_table_submenu_page_callback() {
 
       include plugin_dir_path(__FILE__) . 'shareholder-table/shareholder-table.php';
       shareholder_employees_list_init();
-
-      // $user = wp_get_current_user();
-      // switch ( $user->roles[0] ) {
-      //       case 'administrator':
-      //             $user_amount = 'product_shareholder_admin_amount';
-      //             break;
-                  
-      //       case 'photographer':
-      //             $user_amount = 'product_shareholder_photographer_amount';
-      //             break;
-
-      //       case 'graphicer':
-      //             $user_amount = 'product_shareholder_graphicer_amount';
-      //             break;
-
-      //       default:
-      //             $user_amount = '';
-      //             break;
-      // }
-
-      // $line_items = tr_query()->table('se7en_wc_order_product_lookup');
-      // $line_items = $line_items->findAll()->orderBy('order_item_id', 'DESC')->groupBy(['variation_id','product_id','order_id'])->get();
-      // foreach( $line_items as $key => $value ) {
-      //       echo 'شناسه: ' . $value->order_item_id . ' | ';
-
-      //       echo 'شناسه محصول: ' . $value->product_id . ' | ';
-      //       if( $value->variation_id == 0 ) {
-      //             $product = tr_query()->table('se7en_posts')->findById($value->product_id)->select('ID', 'post_title')->get();
-      //       } else {
-      //             $product = tr_query()->table('se7en_posts')->findById($value->variation_id)->select('ID', 'post_title')->get();
-      //       }
-      //       echo 'نام محصول: ' . $product['post_title'] . ' | ';
-      //       echo 'شناسه متغیر: ' . $value->variation_id . ' | ';
-
-      //       echo 'شناسه خریدار: ' . $value->customer_id . ' | ';
-      //       $customer = tr_query()->table('se7en_users')->findById($value->customer_id)->select('display_name')->get();
-
-      //       $where = [
-      //             [
-      //                   'column' => 'meta_key',
-      //                   'operator' => '=',
-      //                   'value' => $user_amount
-      //             ],
-      //             // 'AND',
-      //             // [
-      //             //       'column' => 'meta_value',
-      //             //       'operator' => '=',
-      //             //       'value' => get_current_user_id()
-      //             // ]
-      //       ];
-      //       $shareholder = tr_query()->table('se7en_postmeta')->setIdColumn('post_id')->findByID($value->product_id)->where($where)->select('meta_value')->get();
-      //       if( $shareholder['meta_value'] ) {
-      //             echo 'درصد کاربر: ' . $shareholder['meta_value'] . ' | ';
-      //             echo 'سهم کاربر: ' . ($value->product_gross_revenue * $shareholder['meta_value']) / 100 . ' | ';
-      //       }
-      //       echo 'شناسه کاربر: ' . $user->ID . ' | ';
-      //       echo 'نام کاربر: ' . $user->display_name . ' | ' . $user->user_login . ' | ';
-
-      //       echo 'شناسه سفارش: ' . $value->order_id . ' | ';
-      //       echo 'مبلغ ناخالص: ' . $value->product_net_revenue . ' | ';
-      //       echo 'مبلغ خالص ' . $value->product_gross_revenue . ' | ';
-      //       echo 'تاریخ سفارش ' . $value->date_created . ' | ';
-
-      //       echo "<hr>";
-      // }
 
 }
--- wp-content/mu-plugins/vendor/typerocket/functions/shareholder-table/shareholder-table.php
+++ wp-content/mu-plugins/vendor/typerocket/functions/shareholder-table/shareholder-table.php
@@ -2,15 +2,6 @@
 
 if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access directly.
 
-// get saved screen meta value
-add_filter('set-screen-option', 'shareholder_table_set_option', 10, 3);
-
-function shareholder_table_set_option($status, $option, $value) {
-
-      return $value;
-
-}
-
 // Plugin menu callback function
 function shareholder_employees_list_init() {
 
@@ -22,23 +13,20 @@ function shareholder_employees_list_init() {
             // Prepare table
             $empTable->prepare_items();
 
-            echo "<form method='get'>";
-                  echo "<input type='hidden' name='page' value='employees_list_table' />";
-                  $empTable->search_box('جستجو', 'search_id');
+            // echo "<form method='get'>";
+            //       echo "<input type='hidden' name='page' value='wc-shareholder' />";
+            //       $empTable->search_box('جستجو', 'search_id');
                   // Display table
-                  $empTable->display();
-            echo "</form>";
-      echo "</div>";
+                  // if( isset($_GET['shareholder_id']) ) {
+                        $empTable->display();
+                  // } else {
+                  //       echo 'لطفاً کاربر دارای سهام فروش مد نظر خود را انتخاب نمایید...';
+                  // }
+      //       echo "</form>";
+      // echo "</div>";
 
 }
 
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                                                //
-//                                        Start Table List                                                        //
-//                                                                                                                //
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Loading table class
 if( !class_exists('WP_List_Table') ) {
@@ -50,33 +38,23 @@ if( !class_exists('WP_List_Table') ) {
 // Extending class
 class Employees_List_Table extends WP_List_Table {
 
-      // private $users_data;
       private $table_data;
 
-      private function get_users_data($search = "") {
+      private function get_shareholder_data($search = "") {
 
-            global $wpdb;
-
-            if (!empty($search)) {
-                  return $wpdb->get_results(
-                        "SELECT ID,user_login,user_email,display_name from {$wpdb->prefix}users WHERE ID Like '%{$search}%' OR user_login Like '%{$search}%' OR user_email Like '%{$search}%' OR display_name Like '%{$search}%'",
-                        ARRAY_A
-                  );
-            } else {
-                  return $wpdb->get_results(
-                        "SELECT ID,user_login,user_email,display_name from {$wpdb->prefix}users",
-                        ARRAY_A
-                  );
-            }
-
+            $line_items = tr_query()->table('se7en_wc_order_product_lookup');
+            $this->table_data = $line_items->findAll()->orderBy('order_item_id', 'DESC')->groupBy(['variation_id','product_id','order_id'])->get();
+            $this->table_data = json_decode($this->table_data);
+            return $this->table_data;
+            
       }
 
       // Define table columns
       function get_columns() {
 
             $columns = array(
-                  'cb'            => '<input type="checkbox" />',
-                  'ID'          => 'شناسه',
+                  // 'cb'            => '<input type="checkbox" />',
+                  // 'ID'          => 'شناسه',
                   'product'     => 'محصول',
                   'customer'    => 'خریدار',
                   'sharehoder'  => 'سهم',
@@ -90,10 +68,10 @@ class Employees_List_Table extends WP_List_Table {
       // Bind table with columns, data and all
       function prepare_items() {
 
-            if ( isset($_GET['page'] ) && isset( $_GET['s']) ) {
-                  $this->users_data = $this->get_users_data($_GET['s']);
+            if ( isset( $_GET['page'] ) && isset( $_GET['s'] ) ) {
+                  $this->table_data = $this->get_shareholder_data($_GET['s']);
             } else {
-                  $this->users_data = $this->get_users_data();
+                  $this->table_data = $this->get_shareholder_data();
             }
 
             $columns = $this->get_columns();
@@ -102,57 +80,53 @@ class Employees_List_Table extends WP_List_Table {
             $this->_column_headers = array($columns, $hidden, $sortable);
 
             /* pagination */
-            $per_page = $this->get_items_per_page('employees_per_page', 20);
+            $per_page = $this->get_items_per_page('shareholder_per_page', 20);
             $current_page = $this->get_pagenum();
-            $total_items = count($this->users_data);
+            $total_items = count($this->table_data);
 
             // edit
-            if (isset($_GET['action']) && $_GET['page'] == "employees_list_table" && $_GET['action'] == "edit") {
-                  $empID = intval($_GET['employee']);
+            // if (isset($_GET['action']) && $_GET['page'] == "wc-shareholder" && $_GET['action'] == "edit") {
+            //       $empID = intval($_GET['employee']);
 
-                  //... do operation
-            }
+            //       //... do operation
+            // }
 
             // delete
-            if (isset($_GET['action']) && $_GET['page'] == "employees_list_table" && $_GET['action'] == "delete") {
-                  $empID = intval($_GET['employee']);
+            // if (isset($_GET['action']) && $_GET['page'] == "wc-shareholder" && $_GET['action'] == "delete") {
+            //       $empID = intval($_GET['employee']);
 
-                  //... do operation
-            }
+            //       //... do operation
+            // }
 
             // bulk action
-            if (isset($_GET['action']) && $_GET['page'] == "employees_list_table" && $_GET['action'] == "delete_all") {
-                  $empIDs = $_GET['user'];
+            // if (isset($_GET['action']) && $_GET['page'] == "wc-shareholder" && $_GET['action'] == "delete_all") {
+            //       $empIDs = $_GET['user'];
                   
-                  //... do operation
-            }
+            //       //... do operation
+            // }
 
-            if (isset($_GET['action']) && $_GET['page'] == "employees_list_table" && $_GET['action'] == "draft_all") {
-                  $empIDs = $_GET['user'];
+            // if (isset($_GET['action']) && $_GET['page'] == "wc-shareholder" && $_GET['action'] == "draft_all") {
+            //       $empIDs = $_GET['user'];
                   
-                  //... do operation
-            }
+            //       //... do operation
+            // }
 
-            $this->users_data = array_slice($this->users_data, (($current_page - 1) * $per_page), $per_page);
+            $this->table_data = array_slice($this->table_data, (($current_page - 1) * $per_page), $per_page);
 
             $this->set_pagination_args(array(
                   'total_items' => $total_items, // total number of items
                   'per_page'    => $per_page // items to show on a page
             ));
 
-            usort($this->users_data, array(&$this, 'usort_reorder'));
-
-            // $this->items = $this->users_data;
-
-
-            $line_items = tr_query()->table('se7en_wc_order_product_lookup');
-            $this->table_data = $line_items->findAll()->orderBy('order_item_id', 'DESC')->groupBy(['variation_id','product_id','order_id'])->get();
+            // $this->table_data = $this->get_shareholder_data();
+            // $this->items = $this->table_data;
             $this->items = $this->table_data;
 
       }
 
       // bind data with column
       function column_default($item, $column_name) {
+
             switch ($column_name) {
 
                   case 'ID':
@@ -233,48 +207,37 @@ class Employees_List_Table extends WP_List_Table {
             $sortable_columns = array(
                   'ID'           => array('ID', false),
                   'product'      => array('product_id', false),
-                  'customer'     => array('customer_id', true),
-                  'sharehoder'   => array('sharehoder_price', true),
-                  'order'        => array('order_price', true),
-                  'order_date'   => array('order_date', true),
+                  'customer'     => array('customer_id', false),
+                  'sharehoder'   => array('sharehoder_price', false),
+                  'order'        => array('order_price', false),
+                  'order_date'   => array('order_date', false),
             );
             return $sortable_columns;
 
       }
 
-      // Sorting function
-      function usort_reorder($a, $b)
-      {
-            // If no sort, default to user_login
-            $orderby = (!empty($_GET['orderby'])) ? $_GET['orderby'] : 'user_login';
-            // If no order, default to asc
-            $order = (!empty($_GET['order'])) ? $_GET['order'] : 'asc';
-            // Determine sort order
-            $result = strcmp($a[$orderby], $b[$orderby]);
-            // Send final sort direction to usort
-            return ($order === 'asc') ? $result : -$result;
-      }
-
       // Adding action buttons to column
-      function column_user_login($item)
-      {
-            $actions = array(
-                  'edit'      => sprintf('<a href="?page=%s&action=%s&employee=%s">Edit</a>', $_REQUEST['page'], 'edit', $item['ID']),
-                  'delete'    => sprintf('<a href="?page=%s&action=%s&employee=%s">Delete</a>', $_REQUEST['page'], 'delete', $item['ID']),
-            );
+      // function column_ID($item) {
 
-            return sprintf('%1$s %2$s', $item['user_login'], $this->row_actions($actions));
-      }
+      //       $actions = array(
+      //             'edit'      => sprintf('<a href="?page=%s&action=%s&employee=%s">Edit</a>', $_REQUEST['page'], 'edit', $item->ID),
+      //             'delete'    => sprintf('<a href="?page=%s&action=%s&employee=%s">Delete</a>', $_REQUEST['page'], 'delete', $item->ID),
+      //       );
+
+      //       return sprintf('%1$s %2$s', $item->ID, $this->row_actions($actions));
+
+      // }
 
       // To show bulk action dropdown
-      function get_bulk_actions()
-      {
-            $actions = array(
-                'edit_all'      => "ویرایش",
-                'draft_all'     => "پیشنویس",
-                'delete_all'    => 'انتقال به زباله‌دان',
-            );
-            return $actions;
-      }
+      // function get_bulk_actions() {
+
+      //       $actions = array(
+      //           'edit_all'      => "ویرایش",
+      //           'draft_all'     => "پیشنویس",
+      //           'delete_all'    => 'انتقال به زباله‌دان',
+      //       );
+      //       return $actions;
+
+      // }
 
 }
