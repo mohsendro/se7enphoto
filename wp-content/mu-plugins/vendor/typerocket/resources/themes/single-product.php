35d2ddd9bc118141d1e57cc0280bd0eb5b06596e
mohsendro <mohsendroo@yahoo.com>
2023-03-14 23:26:05 +0330

    two commit 1401/12/23

---
+++ wp-content/mu-plugins/vendor/typerocket/resources/themes/single-product.php
@@ -0,0 +1,54 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access directly.
+
+?>
+
+<?php get_header(); ?>
+
+<!-- Gallery Single Start -->
+<section id="gallery-single" class="container gallery-single">
+    <div class="row">
+        <div class="col-12 info-single">
+
+			<?php
+				/**
+				 * woocommerce_before_main_content hook.
+				 *
+				 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+				 * @hooked woocommerce_breadcrumb - 20
+				 */
+				do_action( 'woocommerce_before_main_content' );
+			?>
+
+				<?php while ( have_posts() ) : ?>
+					<?php the_post(); ?>
+
+					<?php wc_get_template_part( 'content', 'single-product' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			<?php
+				/**
+				 * woocommerce_after_main_content hook.
+				 *
+				 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+				 */
+				do_action( 'woocommerce_after_main_content' );
+			?>
+
+			<?php
+				/**
+				 * woocommerce_sidebar hook.
+				 *
+				 * @hooked woocommerce_get_sidebar - 10
+				 */
+				do_action( 'woocommerce_sidebar' );
+			?>
+			
+		</div>
+    </div>
+</section>
+<!-- Gallery Single End -->
+
+<?php get_footer(); ?>
